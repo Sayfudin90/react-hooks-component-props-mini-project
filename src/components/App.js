24d2ee51333047b3056d33cdd5d26f3867682643
Sayfudin90 <Seifdin62@gmail.com>
2025-01-18 18:02:56 +0300

    Fix About component and its tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React from "react";
-import blogData from "../data/blog";
-
-console.log(blogData);
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name="Underreacted" />
+      <About image="https://via.placeholder.com/215" about="This is my blog" />
+      <ArticleList
+        posts={[
+          { id: 1, title: "Post 1", date: "January 1, 2023", preview: "Preview 1" },
+          { id: 2, title: "Post 2", date: "February 1, 2023", preview: "Preview 2" },
+          { id: 3, title: "Post 3", preview: "Preview 3" },
+        ]}
+      />
     </div>
   );
 }
